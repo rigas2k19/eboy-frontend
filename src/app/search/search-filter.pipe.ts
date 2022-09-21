2650974b94d45bea32b1a2a3ce9fd5e05db50436
rigas2k19 <rigas2k19@yandex.com>
2022-09-21 14:35:41 +0300

    map

--- src/app/search/search-filter.pipe.ts
+++ src/app/search/search-filter.pipe.ts
@@ -5,43 +5,36 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchFilterPipe implements PipeTransform {
 
-  transform(items: any[], keyword: any, properties: string[]): any[] {
-    if (!keyword){
-      return items;
-    }
-    return items.filter(item => {
-      var itemFound: Boolean = false;
-      for (let i = 0; i < properties.length; i++) {
-        if (item[properties[i]].toString().toLowerCase().indexOf(keyword.toString().toLowerCase()) !== -1) {
-          itemFound = true;
-          break;
+  transform(list: any[], value: string[], key: string []): any {
+    value.forEach((name: any, index) => {
+      console.log(value);
+      console.log(name);
+      console.log(key[index]);
+      if(key[index] != 'description' && key[index] != 'name'){
+        if (name) {
+          list = list.filter((item) => {
+            return (item[key[index]]
+              .toString()
+              .toLowerCase()
+              .indexOf(name.toString().toLowerCase()) !== -1)
+          });
         }
-      }
-      return itemFound;
-    });
-  }
+      }else{
+        if (name) {
+          list = list.filter((item) => {
+            return (item['name']
+              .toString()
+              .toLowerCase()
+              .indexOf(name.toString().toLowerCase()) !== -1 || item['description']
+              .toString()
+              .toLowerCase()
+              .indexOf(name.toString().toLowerCase()) !== -1 )
+          });
+        }
 
-/*
-  transform(list: any[], value: [], key: []): any {
-    value.forEach((name, index) => {
-      if (name) {
-        list = list.filter((item) => {
-          return (item[key[index]]
-            .toString()
-            .toLowerCase()
-            .indexOf(name.toString().toLowerCase()) !== -1)
-        });
       }
+
     });
     return list;
   }
-
-
-  transform(value: any[], filters:any):any {
-    return value.filter(item =>{
-      return (item.name.toLowerCase().indexOf(filters.name.toLowerCase()) >= 0
-      && filters.category.toLowerCase().indexOf(filters.))
-    })
-  }
-*/
 }
